27bfa1daa1aead5cdec28974cf1f9018c7c08301
Robin <>
2020-07-21 19:20:58 +0200

    Setup new vim on osx

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 \cp ./dotfiles/init.vim ~/.config/nvim/init.vim
 \cp ./dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json 
 \cp ./dotfiles/zshrc ~/.zshrc 
-\cp ./dotfiles/vimrc ~/.vimrc 
+\cp ./dotfiles/vimrc ~/.vim/vimrc 
 \cp ./dotfiles/tmux.conf ~/.tmux.conf 
--- get_files.sh
+++ get_files.sh
@@ -3,5 +3,5 @@
 \cp ~/.config/nvim/init.vim ./dotfiles/init.vim
 \cp ~/.config/nvim/coc-settings.json ./dotfiles/coc-settings.json
 \cp ~/.zshrc ./dotfiles/zshrc
-\cp ~/.vimrc ./dotfiles/vimrc
 \cp ~/.tmux.conf ./dotfiles/tmux.conf
+\cp ~/.vim/vimrc ./dotfiles/vimrc
